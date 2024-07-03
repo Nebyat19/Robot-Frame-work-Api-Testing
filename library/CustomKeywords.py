962d4782b2fd17a5f29e6ddf5c92503f3e2e2a3d
Nebyat Bekele <nebyat.bekele@aastustudent.edu.et>
2024-07-03 10:48:52 +0300

    header cookie changed

--- library/CustomKeywords.py
+++ library/CustomKeywords.py
@@ -2,13 +2,10 @@
 
 import requests
 import ast
-
 class CustomKeywords:
-    def make_request(self, method, url, headers, payload=None):
-        
+    def make_request(self, method, url, headers, payload=None):        
         if isinstance(headers, str):
-            headers = ast.literal_eval(headers)
-            
+            headers = ast.literal_eval(headers)           
         response = requests.request(method, url, headers=headers, json=payload)
         response.raise_for_status()
         return {
